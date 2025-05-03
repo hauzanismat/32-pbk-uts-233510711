6e24bb3dce829ed7c321fd7b7b44111696ed4eda
hauzanismat <hauzanismatmuzaki@student.uir.ac.id>
2025-05-03 16:56:29 +0700

    filter item

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const items = ref([])
 const newItem = ref('')
+const filter = ref('all')
 
 const addItem = () => {
   if (newItem.value.trim() !== '') {
@@ -23,15 +24,31 @@ const toggleCompleted = (item) => {
   item.completed == !item.completed
 }
 
+const filteredItems = computed(() => {
+  if (filter.value === 'all') {
+    return items.value
+  } else if (filter.value === 'completed') {
+    return items.value.filter(item => item.completed)
+  } else if (filter.value === 'uncompleted') {
+    return items.value.filter(item => !item.completed)
+  }
+  return items.value
+})
+
 </script>
 
 <template>
   <div>
     <input type="text" v-model="newItem" @keyup.enter="addItem" />
     <button @click="addItem">Tambahkan</button>
+    <select v-model="filter">
+      <option value="all">Semua</option>
+      <option value="completed">Selesai</option>
+      <option value="uncompleted">Belum Selesai</option>
+    </select>
 
     <ul>
-      <li v-for="item in items" :key="item.id">
+      <li v-for="item in filteredItems" :key="item.id">
         <input type="checkbox" v-model="item.completed" @change="toggleCompleted(item)" />
         {{ item.text }}
         <button @click="removeItem(item)">Hapus</button>
